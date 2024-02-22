bdfb752db1e371ed945a0ac6b19819738256320f
Sebastian Thomsen <seba9323@mail.hansenberg.dk>
2024-02-22 11:01:30 +0100

    Brugere

---
+++ app/backend/classes/Administrator.php
@@ -0,0 +1,37 @@
+<?php 
+require_once 'User.php';
+
+class Administrator extends User {
+    public function __construct() {
+        $this->setRole('admininstrator');
+    }
+
+    public function update($fields = array(), $id = null) {
+        if (!$id && $this->isLoggedIn()) {
+            $id = $this->data()->user_id;
+        }
+
+        if (!$this->_db->update('users', 'user_id', $id, $fields)) {
+            throw new Exception('Unable to update the user.');
+        }
+    }
+
+    public function create($fields = array()) {
+        if (!$this->_db->insert('users', $fields)) {
+            throw new Exception("Unable to create the user.");
+        }
+    }
+
+    public function find($user = null) {
+        if ($user) {
+            $field = (is_numeric($user)) ? 'user_id' : 'username';
+            $data = $this->_db->get('users', array($field, '=', $user));
+
+            if ($data->count()) {
+                $this->_data = $data->first();
+                return true;
+            }
+        }
+        return false;
+    }
+}
